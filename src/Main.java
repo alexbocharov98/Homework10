a2803f98b5d5cb77e401fb4040cd2cd23169071c
alexbocharov98 <a.bocharov34@yandex.ru>
2024-10-16 20:55:33 +0300

    Homework10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("task1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("task2");
+
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("task3");
+
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
     }
 }
